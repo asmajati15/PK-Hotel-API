eb891ff011e45420ed02da2c31a537bcf4d2d278
asmajati15 <asmajati.cooler15@gmail.com>
2022-10-13 14:14:43 +0700

    Adding Rooms API

---
+++ app/Http/Controllers/RoomController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Room;
+use Illuminate\Http\Request;
+
+class RoomController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $room = Room::all();
+        return response()->json(['data' => $room], 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $room = new Room;
+        $room->type = $request->type;
+        $room->description = $request->description;
+        $room->price = $request->price;
+        $room->type = $request->type;
+        $saved = $room->save();
+
+        if ($saved) {
+            return response()->json(['message' => 'Room added successfully.'], 201);
+        } else {
+            return response()->json(['message' => 'Something went wrong.'], 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Room  $room
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $room = Room::find($id);
+        return response()->json(['data' => $room], 302);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Room  $room
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Room $room)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Room  $room
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $type = $request->type;
+        $description = $request->description;
+        $price = $request->price;
+
+        $room = Room::find($id);
+        $room->type = $type;
+        $room->description = $description;
+        $room->price = $price;
+        $room->save();
+
+        // $room->save([
+        //     'type' => $request->type,
+        //     'description' => $request->description,
+        //     'price' => $request->price,
+        //     'type' => $request->type
+        // ]);
+        return response()->json(['message' => 'Room updated successfully.'], 201);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Room  $room
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Room $id)
+    {
+        $room = Room::find($id);
+        $room->each->delete();
+        return response()->json(['message' => 'Room delete successfully.'], 202);
+    }
+}
---
+++ database/migrations/2022_10_13_021953_create_invoices_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->date('check_in');
+            $table->date('check_out');
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('room_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('room_id')->references('id')->on('rooms');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
